fd948947a0b7f71e181a464ec48ab9efeef9d494
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-03-30 20:36:16 +0430

    fix answer : Remove Duplicates From Sorted Array
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- LeetCode/Remove_Duplicates_from_Sorted_Array/rdfs_test.go
+++ LeetCode/Remove_Duplicates_from_Sorted_Array/rdfs_test.go
@@ -1,24 +1,37 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestCase1(t *testing.T) {
 	nums := []int{1, 2, 3}
-	if removeDuplicates(&nums) != 3 &&
-		reflect.DeepEqual(nums, []int{1, 2, 3}) {
+	ans := []int{1, 2, 3}
+	l := removeDuplicates(nums)
+	if len(ans) != l {
+		t.Error("Wrong Length!")
+	}
 
-		t.Error("Wrong Answer!")
+	for i := 0; i < l; i++ {
+		if ans[i] != nums[i] {
+			t.Error("Wrong Answer!")
+			return
+		}
 	}
 }
 
 func TestCase2(t *testing.T) {
 	nums := []int{1, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6}
-	if removeDuplicates(&nums) != 6 &&
-		reflect.DeepEqual(nums, []int{1, 2, 3, 4, 5, 6}) {
+	ans := []int{1, 2, 3, 4, 5, 6}
+	l := removeDuplicates(nums)
 
-		t.Error("Wrong Answer!")
+	if len(ans) != l {
+		t.Error("Wrong Length!")
+	}
+
+	for i := 0; i < l; i++ {
+		if ans[i] != nums[i] {
+			t.Error("Wrong Answer!")
+		}
 	}
 }
